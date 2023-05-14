1374b66b01c9c7d67b50de588697901c3b005675
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-14 09:31:24 +0200

    Updated the code
    Used 'use instead of '#[macro_use]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-#[macro_use]
-extern crate rocket;
+use rocket::{self, get, routes};
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!\n"
+fn welkom() -> &'static str {
+    "Welkom op mijn site!!\n"
 }
 
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build().mount("/", routes![hello]).launch().await;
+    let _ = rocket::build().mount("/", routes![welkom]).launch().await;
 }
