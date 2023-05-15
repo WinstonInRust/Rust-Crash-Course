0790d4e37493e3e8569db2673fb73b5c1727525e
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-15 15:06:08 +0200

    updated the code with json

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // Macro's are used to define routes and the main function
-use rocket::{self, get, routes}; // import of rocket crate and macro's
+use ::rocket::serde::json::{json, Value};
+use rocket::{self, get, routes}; // import of rocket crate and macro's // import of json macro // import of json value
 
 #[get("/")] // macro to define a route
-fn welkom() -> &'static str {
-    "Welkom op mijn site!!\n"
+fn welkom() -> Value {
+    json!("Welkom op mijn site!!")
 }
 
 #[rocket::main] // macro to define the main function
