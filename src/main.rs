9eb3d9da9bd897f0d8eb1c2f9cabd95e5622eb32
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-16 10:16:07 +0200

    Added BasicAuth base64
    base64 Basic Authentication for the points

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,55 @@
 // Macro's are used to define routes and the main function
 use ::rocket::serde::json::{json, Value};
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::{self, catch, catchers, delete, get, post, put, response::status, routes}; // import of rocket crate and macro's // import of json macro // import of json value
+
+pub struct BasicAuth {
+    pub username: String,
+    pub password: String,
+}
+
+impl BasicAuth {
+    fn from_autherization_header(header: &str) -> Option<BasicAuth> {
+        let split = header.split_whitespace().collect::<Vec<_>>();
+        if split.len() != 2 {
+            return None;
+        }
+        if split[0] != "Basic" {
+            return None;
+        }
+
+        Self::from_base64_encoded(split[1])
+    }
+
+    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
+        let decoded = base64::decode(base64_string).ok()?;
+        let decoded = String::from_utf8(decoded).ok()?;
+        let split = decoded.split(":").collect::<Vec<_>>();
+        if split.len() != 2 {
+            return None;
+        }
+        Some(BasicAuth {
+            username: split[0].to_string(),
+            password: split[1].to_string(),
+        })
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for BasicAuth {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let auth_header = request.headers().get_one("Authorization");
+        if let Some(auth_header) = auth_header {
+            if let Some(auth) = Self::from_autherization_header(auth_header) {
+                return Outcome::Success(auth);
+            }
+        }
+        Outcome::Failure((Status::Unauthorized, ()))
+    }
+}
 
 #[catch(404)] // catching a 404 error
 fn not_found() -> Value {
