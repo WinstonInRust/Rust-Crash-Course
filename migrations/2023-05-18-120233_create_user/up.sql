9b8620ffb33935f087b2d326add03326bb8ca33a
WinstonInRust <rustprogrammeren@gmail.com>
2023-05-18 14:36:09 +0200

    Cleaning up

--- migrations/2023-05-18-120233_create_user/up.sql
+++ migrations/2023-05-18-120233_create_user/up.sql
@@ -1,8 +1,7 @@
 CREATE TABLE "user" (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
